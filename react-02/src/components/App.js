e78d2e121b1f67c8caf331c31997d3688aac08a5
Mário Antônio do Amaral Rodeghiero <mario.rodeghiero@gmail.com>
2019-02-11 22:10:38 -0200

    edit: onSearch function
    add: authentication wiht axios

--- react-02/src/components/App.js
+++ react-02/src/components/App.js
@@ -1,27 +1,23 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import axios from 'axios'
 import '../App.css';
 import SearchBar from './SearchBar'
 
 class App extends Component {
+
+  onSearchSubmit(term) {
+    axios.get('https://api.unsplash.com/search/photos', {
+       params: {query: term},
+       headers: {
+         Authorization: 'Client-ID 545ba910c3a8ab7083c164f8427dfb4d653d83d12e8bc8b7228d202b4290a218'
+       }
+    })
+    console.log("Teste", term)
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <SearchBar/>
+      <div className="ui container">
+        <SearchBar onSubmit={this.onSearchSubmit}/>
       </div>
     );
   }
--- react-02/src/components/SearchBar.js
+++ react-02/src/components/SearchBar.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
 
 export default class SearchBar extends Component {
+    state = {term: ''}
+
+    onFormSubmit = (event) => {
+        
+        this.props.onSubmit(this.state.term)
+        event.preventDefault();
+    }
   render() {
     return (
       <div className="ui segment">
-        <form>
-            <input type="text"/>
+        <form onSubmit={this.onFormSubmit} className="ui form">
+            <div className="field">
+                <label>Image Search</label>
+                <input 
+                    value={this.state.term}
+                    onChange={ e => this.setState({term: e.target.value})}
+                    type="text"/>
+            </div>
         </form>
       </div>
     )
