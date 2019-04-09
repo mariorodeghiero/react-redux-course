4ca1778ad6f6c39ea7bf95cd5ce7c57a5509c9cb
Mário Antônio do Amaral Rodeghiero <mario.rodeghiero@gmail.com>
2019-04-08 22:47:13 -0300

    add: songlist component and configured provider, store

--- react-redux-songs/src/App.js
+++ react-redux-songs/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import { selectSong } from './actions'
+import SongList from './components/SongList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Songos</p>
+        <SongList></SongList>
       </div>
     );
   }
--- react-redux-songs/src/reducers/index.js
+++ react-redux-songs/src/reducers/index.js
@@ -1,16 +1,23 @@
+import { combineReducers } from 'redux'
+
 const songsReducer = () => {
-    return [
-      {title: 'Song 1', duratuion: '2:30'},
-      {title: 'Song 2', duratuion: '4:00'},
-      {title: 'Song 3', duratuion: '3:30'},
-      {title: 'Song 4', duratuion: '4:23'},
-    ]
+  return [
+    { title: 'Song 1', duratuion: '2:30' },
+    { title: 'Song 2', duratuion: '4:00' },
+    { title: 'Song 3', duratuion: '3:30' },
+    { title: 'Song 4', duratuion: '4:23' },
+  ]
 }
 
-const selectedSongReducer = (selectedSong=null, action) => {
+const selectedSongReducer = (selectedSong = null, action) => {
   if (action.type === 'SONG_SELECTED') {
     return action.payload;
   }
 
   return selectedSong;
 }
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+})
