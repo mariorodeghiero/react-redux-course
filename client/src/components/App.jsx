f5ff34246f8400880518cbf02bb30bf6a3c334d9
Mário Antônio do Amaral Rodeghiero <mario@vanhack.com>
2020-01-03 16:16:51 -0300

    add: routes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-
-const PageOne = () => {
-  return <div>PageOne</div>;
-};
-
-const PageTwo = () => {
-  return <div>PageTwo</div>;
-};
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import StreamCreate from './streams/StreamCreate'
+import StreamEdit from './streams/StreamEdit'
+import StreamShow from './streams/StreamShow'
+import StreamDelete from './streams/StreamDelete'
+import StreamList from './streams/StreamList'
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/two" component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
