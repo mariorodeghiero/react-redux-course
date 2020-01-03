cb9aeddff7f1a37e9e05ed5c6fae3d4706d855f8
Mário Antônio do Amaral Rodeghiero <mario@vanhack.com>
2020-01-02 21:52:13 -0300

    delete: files

---
+++ blog/src/components/PostList.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class PostList extends Component {
+
+  render() {
+    return (
+      <div>
+        PostList
+      </div>
+    )
+  }
+}
+
+export default PostList
