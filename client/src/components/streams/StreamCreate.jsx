ca79bde4b020273cb9325d8cee57d78237e12a14
Mário Antônio do Amaral Rodeghiero <mario@vanhack.com>
2020-01-13 22:50:15 -0300

    add: form using redux-form

--- client/src/components/streams/StreamCreate.jsx
+++ client/src/components/streams/StreamCreate.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Field, reduxForm } from "redux-form";
 
+class StreamCreate extends React.Component {
+  renderInput({ input }) {
+    return (
+      <input {...input}/>
+    );
+  }
 
- const StreamCreate = () => {
-  return (
-    <div>StreamCreate</div>
-  );
+  render() {
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
 }
 
-export default StreamCreate
+export default reduxForm({
+  form: "streamCreate"
+})(StreamCreate);
