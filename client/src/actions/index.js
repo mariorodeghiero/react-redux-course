98b1f3f5dcde49dba7fbfb5600745fd5dffc87f6
Mário Antônio do Amaral Rodeghiero <mario@vanhack.com>
2020-01-15 22:55:46 -0300

    add: new types and fetchStreams action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import streams from '../apis/streams'
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types'
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM, FETCH_STREAMS, EDIT_STREAM, FETCH_STREAM, DELETE_STREAM } from './types'
 
 export const signIn = (userId) => {
   return {
@@ -18,6 +18,14 @@ export const createStream = (formValues) => async (dispatch) => {
   const response = await streams.post('/streams', formValues)
   dispatch({
     type: CREATE_STREAM,
+    payload: response.data
+  })
+}
+
+export const fetchStreams = (formValues) => async (dispatch) => {
+  const response = await streams.post('/streams', formValues)
+  dispatch({
+    type: FETCH_STREAMS,
     payload: response.data
   })
 }
